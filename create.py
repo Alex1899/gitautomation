7a3900eb2e1e07d6998e20eece20f4216f242ad7
alex1899 <kvashilavaaleko@gmail.com>
2020-08-21 00:08:44 +0400

    added new token

--- create.py
+++ create.py
@@ -5,13 +5,14 @@ import requests
 
 def create():
     repoName = str(sys.argv[1])
-    headers = {'Authorization': 'token f8eb9975b3ebee91b42eb3b052d0158455ec1867'}
+    headers = {'Authorization': 'token c4134fd3bf4b16c293c2ee70348b69c92b4ff18e'}
     req = requests.post('https://api.github.com/user/repos', headers=headers, json={'name': repoName})
    
     if('name' in req.json().keys()):
         print("Starting...")
     else:
         print("Fail")
+        print(req.json())
     sys.exit(0)
 
 
